29c8771aaa73530634fc63443d4c528d651ced53
Giulia Damascena <100076009+giuliadamascena@users.noreply.github.com>
2025-07-18 12:55:37 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('notes_app.urls')),
-
+    path('', views.note_list, name='note_list'),
+    path('create/', views.note_create, name='note_create'),
+    path('update/<int:pk>/', views.note_update, name='note_update'),
+    path('delete/<int:pk>/', views.note_delete, name='note_delete'),
 ]
